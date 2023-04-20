62fafb20bbcd6ea87a915421be5e6211962d49a4
KokoaTree <4kakao1@daum.net>
2023-04-20 17:08:10 +0900

    feat: codeup1013 - codeup 1013 풀이 수정

--- src/codeup/codeup1013.java
+++ src/codeup/codeup1013.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class codeup1013 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.printf("%d, %d", scan.nextInt(), scan.nextInt());
+        System.out.printf("%d %d", scan.nextInt(), scan.nextInt());
     }
 }
