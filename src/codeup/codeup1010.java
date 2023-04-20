624760ec30497de48738d676114ee159de27fc3e
KokoaTree <4kakao1@daum.net>
2023-04-20 12:22:07 +0900

    feat: codeup Exercise - codeup 1010 풀이

---
+++ src/codeup/codeup1010.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1010 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.printf("%d", num.nextInt());
+    }
+}
