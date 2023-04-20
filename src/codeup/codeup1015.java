bdaa942bbf6ec9826fcf4364b39de58e2d520fc2
KokoaTree <4kakao1@daum.net>
2023-04-20 15:48:22 +0900

    feat: codeup 1015 - codeup 1015 풀이

---
+++ src/codeup/codeup1015.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1015 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("%.2f", scan.nextFloat());
+    }
+}
