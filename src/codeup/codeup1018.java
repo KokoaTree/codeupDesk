7ec793311139c2b34840c5436c22a27cf4959cb2
KokoaTree <4kakao1@daum.net>
2023-04-21 11:07:07 +0900

    feat: codeup1018 - codeup 1018 풀이

---
+++ src/codeup/codeup1018.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.next();
+
+        System.out.printf("%s", time);
+
+    }
+}
