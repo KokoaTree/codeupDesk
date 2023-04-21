e82bbdaa73bad65269035e1f001224d47340c703
KokoaTree <4kakao1@daum.net>
2023-04-21 10:47:31 +0900

    feat: codeup1017 - codeup 1017 풀이

---
+++ src/codeup/codeup1017.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.printf("%d %d %d", num, num, num);
+    }
+}
