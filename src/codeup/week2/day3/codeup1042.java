4b888c9369bc05b84c563987d3158bc45a870255
KokoaTree <4kakao1@daum.net>
2023-04-26 14:11:10 +0900

    feat: codeup1042-1043 - codeup 1042 풀이 - codeup 1043 풀이

---
+++ src/codeup/week2/day3/codeup1042.java
@@ -0,0 +1,15 @@
+package codeup.week2.day3;
+
+import java.util.Scanner;
+
+public class codeup1042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.printf("%d", num1 / num2);
+    }
+
+}
---
+++ src/codeup/week2/day3/codeup1043.java
@@ -0,0 +1,15 @@
+package codeup.week2.day3;
+
+import java.util.Scanner;
+
+public class codeup1043 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        System.out.printf("%d", val1 % val2);
+    }
+
+}
