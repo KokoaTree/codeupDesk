244eba57d9e6bfae05b742bbe58c6925d24c2e18
KokoaTree <4kakao1@daum.net>
2023-04-27 11:21:40 +0900

    feat: codeup1049-1052 - codeup 1049 풀이 - codeup 1050 풀이 - codeup 1051 풀이 - codeup 1052 풀이

---
+++ src/codeup/week2/day3/codeup1051.java
@@ -0,0 +1,19 @@
+package codeup.week2.day3;
+
+import java.util.Scanner;
+
+public class codeup1051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        System.out.println(val1 <= val2 ? 1 : 0);
+//        if(val1 <= val2){
+//            System.out.println(1);
+//        }
+//        else
+//            System.out.println(0);
+    }
+}
