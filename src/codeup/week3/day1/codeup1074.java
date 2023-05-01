eaef99ec4b0fecca90326b44cefd74d95875f22e
KokoaTree <4kakao1@daum.net>
2023-05-01 12:31:27 +0900

    feat: codeup1073~1075실습 - codeup1073 풀이 - codeup1074 풀이 - codeup1075 풀이

--- src/codeup/week3/day1/codeup1074.java
+++ src/codeup/week3/day1/codeup1074.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class codeup1074 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         for(int num = sc.nextInt(); num <= 100; num--){
             System.out.println(num);
             if(num == 1) {
