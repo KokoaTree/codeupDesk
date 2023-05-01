b7342ec5d4ba1958d6b0169bbb9f439c6eda533b
KokoaTree <4kakao1@daum.net>
2023-05-01 11:02:41 +0900

    feat: codeup 실습 - codeup1103 풀이 - codeup1104 풀이 - codeup1071 풀이 - codeup1072 풀이

---
+++ src/codeup/week3/day1/codeup1103.java
@@ -0,0 +1,8 @@
+package codeup.week3.day1;
+
+public class codeup1103 {
+    public static void main(String[] args) {
+
+        System.out.printf("\"c:\\test\"\n");
+    }
+}
---
+++ src/codeup/week3/day1/codeup1104.java
@@ -0,0 +1,9 @@
+package codeup.week3.day1;
+
+public class codeup1104 {
+    public static void main(String[] args) {
+        int minVal = Integer.MIN_VALUE;
+        int maxVal = Integer.MAX_VALUE;
+        System.out.printf("%d %d", minVal, maxVal);
+    }
+}
